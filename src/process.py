0641341765f7e077670bf834bfa3d7c5cb0716c0
liz <lizjunkbox@gmail.com>
2022-02-17 20:59:33 -0500

    Added LoadDet2Model Function

--- src/process.py
+++ src/process.py
@@ -4,8 +4,10 @@ import numpy as np
 import pdf2image
 import sys
 import logging
+import layoutparser as lp
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("Process")
+
 
 def ConvertPDF(file, pagenum):
     """
@@ -18,3 +20,13 @@ def ConvertPDF(file, pagenum):
     return pdf
 
 
+def LoadDet2Model(DET_MODEL_PATH, LABEL_MAP):
+    """
+    Loads the selected Delectron2 model
+        DET_MODEL_PATH: Model path 
+        LABEL_MAP: "Label Map" used by model, see LayoutParser website for more information 
+    """
+    model = lp.Detectron2LayoutModel(
+        config_path = DET_MODEL_PATH, 
+        label_map = LABEL_MAP)
+    return model
