d54353663be800d99e9b4bd26b751c83f5e7eff8
liz <lizjunkbox@gmail.com>
2022-02-18 08:27:46 -0500

    created to_pos_id function

--- src/process.py
+++ src/process.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("Process")
 
 
-def ConvertPDF(file, pagenum):
+def convert_PDF(file, pagenum):
     """
     Converts pdf to numpy array
         file: The file path of the PDF
@@ -20,7 +20,7 @@ def ConvertPDF(file, pagenum):
     return pdf
 
 
-def LoadDet2Model(DET_MODEL_PATH, LABEL_MAP):
+def load_det2_model(DET_MODEL_PATH, LABEL_MAP):
     """
     Loads the selected Delectron2 model
         DET_MODEL_PATH: Model path 
@@ -31,3 +31,25 @@ def LoadDet2Model(DET_MODEL_PATH, LABEL_MAP):
         label_map = LABEL_MAP)
     log.info('Loaded Detectron2 model with LayoutParser')
     return model
+
+def to_pos_id(y_1 = float, y_2 = float, pagenum = int, docheight = 3850) -> float:
+    """
+    Returns a position ID (float) of its position on the page as a decimal from 0 to 1, added on top of the pagenum
+
+    Ex: A page that is 500px tall, with y_1 = 225 and y_2 = 275 on page 5. 
+    Average height on page is 250px, so returns .5. Added to the page number gives the Position ID
+    of 5.5. This is used to have a scalar value for the absolute position of objects in an entire document, instead of just simple pages. 
+    Intended to be used to match up titles and tables that get cut off/on different pages from each other. 
+
+    Arguments: 
+        y_1: Lower Y value of the selection 
+        y_2: Higher Y value of the selection 
+        pagenum: Page number of the page where the selection came from 
+        docheight: Height, in pixels, of the array of the document. Defaults to 3850, as that is dpi = 350 of the PDF
+    
+    """
+    return float(pagenum) + (np.mean(y_1, y_2)/docheight)
+
+to_pos_id()
+#if __name__ == '__main__':
+
